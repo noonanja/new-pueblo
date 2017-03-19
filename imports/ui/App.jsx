21077df3c00b95d8187039a40108f0eae494fee2
Jacob Noonan <JacobNoonan@vagvlan506.165.wlan.law.upenn.edu>
2017-03-19 13:05:02 -0400

    mean daily requirements spinner number

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,18 +3,19 @@ import { createContainer } from 'meteor/react-meteor-data';
 
 import ReactDOM from 'react-dom';
 
+
 import Range from 'rc-slider/lib/Range';
 import Slider from 'rc-slider';
 const Handle = Slider.Handle;
 const Tooltip = require('rc-tooltip');
-
-
 import 'rc-slider/assets/index.css';
 
 const activeMax = 180;
 const defaultValues= [40, 80, 120];
 const marks = {0: '0'};
 marks[activeMax]= `${activeMax}`;
+
+const defaultRequirements = 12;
 
 
 // App component - represents the whole app
@@ -24,7 +25,11 @@ class App extends Component {
 
     this.state = {
       values: defaultValues,
+      requirements: defaultRequirements,
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handle(props) {
@@ -53,16 +58,36 @@ class App extends Component {
   renderRange() {
     return (
       <Range
-        className="range" min={0} max={activeMax} defaultValue={defaultValues}
-        pushable={true} step={5} included={false} marks ={marks}
-        ref= "range"
-        handle= {this.handle.bind(this)}
-        onChange= {this.updateTipValues.bind(this)}
-        />
+        handle= {this.handle.bind(this)} onChange= {this.updateTipValues.bind(this)}
+        ref= "range" className="range" min={0} max={activeMax} pushable={true}
+        defaultValue={defaultValues} step={5} included={false} marks ={marks} />
+    )
+  }
+
+  handleChange(event) {
+    this.setState({requirements: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Form was completed: ' + this.state.requirements);
+    event.preventDefault();
+  }
+
+  renderRequirements() {
+    // <input type="submit" value="Submit" />
+    return (
+      <label className="mean-daily">
+        <p>Mean Daily User Energy Requirements</p>
+        <input type="number" step="2" value={this.state.requirements} required
+        min={defaultRequirements-4} max={defaultRequirements+4}
+        onChange={this.handleChange} />
+      </label>
     )
   }
 
   render() {
+    // <form onSubmit={this.handleSubmit()}>
+    // </form>
     return (
       <div id="canvas">
         <div id="header-wrapper">
@@ -74,29 +99,29 @@ class App extends Component {
         </div>
 
         <div className="container">
-          <div className="row">
-            <h5> Demand-Side Users </h5>
-                {this.renderRange()}
-                <p className="mean-daily"> Mean Daily User Energy Requirements </p>
-          </div>
+            <div className="row">
+              <h5> Demand-Side Users </h5>
+                  {this.renderRange()}
+                  {this.renderRequirements()}
+            </div>
 
-          <div className="row">
-            <div className="one-half column">
-              <h5> Energy Storage </h5>
+            <div className="row">
+              <div className="one-half column">
+                <h5> Energy Storage </h5>
+              </div>
+              <div className="one-half column">
+                <h5> Energy Production </h5>
+              </div>
             </div>
-            <div className="one-half column">
-              <h5> Energy Production </h5>
-            </div>
-          </div>
 
-          <div className="row">
-            <div className="one-half column">
-              <h5> Aggregate Load </h5>
+            <div className="row">
+              <div className="one-half column">
+                <h5> Aggregate Load </h5>
+              </div>
+              <div className="one-half column">
+                <h5> Price/ Unit Energy </h5>
+              </div>
             </div>
-            <div className="one-half column">
-              <h5> Price/ Unit Energy </h5>
-            </div>
-          </div>
 
         </div>
 
