76f9d76c0fd916aafed0f20ed99b64f4997e7c14
Jacob Noonan <JacobNoonan@vagvlan506.0516.wlan.sas.upenn.edu>
2017-04-16 15:57:35 -0400

    optimal solution debugging

--- server/.scripts/argmin.py
+++ server/.scripts/argmin.py
@@ -28,7 +28,7 @@ class Simulation(object):
     strategy given the aggregate loads in the current iteration
     """
     def __init__(self,
-                cEF,
+                 cEF,
                  dEF,
                  c,
                  mCR,
@@ -71,14 +71,69 @@ class Simulation(object):
         self.activeAggLoad = activeAggLoad;
         self.activeLoads = activeLoads;
 
-    def simulate(self):
-        load = self.activeLoads[4]
-        # for load in self.activeLoads:
-        otherActiveAgg = np.subtract(self.activeAggLoad, load['l'])
-        otherAgg = np.add(self.passiveLoadValues, otherActiveAgg)
-        if (load['hasStore'] and not load['hasGen']):
-            s = self.storer_minimize(load['e'], otherAgg)
-            print s
+        self.qZero = .25*c;
+        self.epsilon = 0.0;
+
+        """
+            Create portion of objective function, a matrix P, and constraint matrices
+            G and h, which all will be shared for each user
+        """
+        self.P = np.zeros((48,48))
+        hour = 1
+        for i in xrange(0, len(self.P) - 1, 2):
+            self.P[i][i]     =  gridK[hour] * self.cEF * self.cEF
+            self.P[i][i+1]   = -gridK[hour] * self.cEF * self.dEF
+            self.P[i+1][i]   = -gridK[hour] * self.cEF * self.dEF
+            self.P[i+1][i+1] =  gridK[hour] * self.dEF * self.dEF
+            hour += 1
+        self.P = 2*self.P
+
+
+        # G (122 x 48) matrix for optimization contraints
+        self.G = np.zeros((122, 48))
+        # h (122 x 1) column vector for optimization contraints
+        self.h = np.zeros((122))
+
+        # hourly charge limit
+        j = 0
+        for i in xrange(0, 24):
+            # (amount charged * cEF) - (amount discharged * dEF) <= mCR
+            self.h[i]      = self.mCR
+            self.G[i][j]   = self.cEF
+            self.G[i][j+1] = self.dEF
+            j+=2
+
+
+        # daily charge and discharge limits
+        for i in xrange(0, 24):
+            # total amount charged or discharged throughout the day,
+            # considering leakage, is below battery capacity (if charged)
+            # and above remaining charge level (if discharging)
+            self.h[i+24] = self.c - self.qZero*(self.lR**(i+1))
+            self.h[i+48] = self.qZero*(self.lR**(i+1))
+            for j in xrange(0, (2*(i+1)), 2):
+                # charge limit
+                self.G[i+24][j]   =  (self.lR**((i)-j/2))*self.cEF
+                self.G[i+24][j+1] = -(self.lR**((i)-j/2))*self.dEF
+                # discharge limit
+                self.G[i+48][j]   =  (self.lR**((i)-j/2))*self.dEF
+                self.G[i+48][j+1] = -(self.lR**((i)-j/2))*self.cEF
+
+        # (remaining new charge - natural leakage) - initial q charge level =
+        # tiny epsilon value
+        self.h[72] = self.epsilon + (1 - self.lR**24)*self.qZero
+        self.h[73] = self.epsilon + (1 - self.lR**24)*self.qZero
+        for j in xrange(0, 48, 2):
+            #  remaining new charge < epsilon + natural leakage
+            self.G[72][j]   =  (self.lR**(23-j/2))*self.cEF
+            self.G[72][j+1] = -(self.lR**(23-j/2))*self.dEF
+            # -remaining new charge < epsilon + natural leakage
+            self.G[73][j]   =  (self.lR**(23-j/2))*self.dEF
+            self.G[73][j+1] = -(self.lR**(23-j/2))*self.cEF
+
+        # hourly charge or discharge must be non-negative
+        for i in xrange(0, 48):
+            self.G[74+i][i] = -1
 
     def storer_minimize(self, e, otherAgg):
         """
@@ -91,52 +146,27 @@ class Simulation(object):
         We will use the standard form of a Quadratic Programming problem below, defined
         here: courses.csail.mit.edu/6.867/wiki/images/a/a7/Qp-cvxopt.pdf
         """
-        P = np.zeros((48,48))
-        h = 1
-        for i in xrange(0, len(P) - 1, 2):
-            P[i][i] = gridK[h] * self.cEF * self.cEF
-            P[i][i+1]= -gridK[h] * self.cEF * self.dEF
-            P[i+1][i] = -gridK[h] * self.cEF * self.dEF
-            P[i+1][i+1] = gridK[h] * self.dEF * self.dEF
-            h += 1
-        # P = matrix()
         q = []
-        h = 1 # prices are indexed from 1
+        hour = 1 # prices are indexed from 1
         for i in xrange(0, 47, 2):
-            q.append( 2*self.cEF*e[h-1]*gridK[h] + gridK[h]*otherAgg[h-1]*self.cEF)
-            q.append(-2*self.dEF*e[h-1]*gridK[h] + gridK[h]*otherAgg[h-1]*self.dEF)
-            h += 1
-        # q = matrix(2 * q)
+            q.append( 2*self.cEF*e[hour-1]*gridK[hour] + gridK[hour]*otherAgg[hour-1]*self.cEF)
+            q.append(-2*self.dEF*e[hour-1]*gridK[hour] + gridK[hour]*otherAgg[hour-1]*self.dEF)
+            hour += 1
+        q = np.array(q)
 
-        G = np.zeros((122, 48))
-        j = 0
-        for i in xrange(0, 24):
-            G[i][j] = self.cEF
-            G[i][j+1] = self.dEF
-            j+=2
-        for i in xrange(0, 24):
-            for j in xrange(0, (2*(i+1)), 2):
-                # charge limit
-                G[i+24][j]   =  (self.lR**((i)-j/2))*self.cEF
-                G[i+24][j+1] = -(self.lR**((i)-j/2))*self.dEF
-                # discharge limit
-                G[i+48][j]   =  (self.lR**((i)-j/2))*self.dEF
-                G[i+48][j+1] = -(self.lR**((i)-j/2))*self.cEF
-        for j in xrange(0, 48, 2):
-            # final charge equals initial charge
-            G[72][j]   =  (self.lR**(23-j/2))*self.cEF
-            G[72][j+1] = -(self.lR**(23-j/2))*self.dEF
-            # final charge equals initial charge
-            G[73][j]   =  (self.lR**(23-j/2))*self.dEF
-            G[73][j+1] = -(self.lR**(23-j/2))*self.cEF
+        return solvers.qp(matrix(self.P), matrix(q), matrix(self.G), matrix(self.h))['x']
 
-        print G[47]
-        print G[72]
-            # G[i] = np.array([])
-        # G = matrix()
-        # h = matrix()
-        #
-        # sol = solvers.qp(P, q, G, h)
+    def simulate(self):
+        load = self.activeLoads[4]
+        # for load in self.activeLoads:
+        otherActiveAgg = np.subtract(self.activeAggLoad, load['l'])
+        otherAgg = np.add(self.passiveLoadValues, otherActiveAgg)
+        if (load['hasStore'] and not load['hasGen']):
+            s = self.storer_minimize(load['e'], otherAgg)
+            print load['userId']
+            print load['hasStore']
+            print load['hasGen']
+            print s
 
 
 def main(args):
